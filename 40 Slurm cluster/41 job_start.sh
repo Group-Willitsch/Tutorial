f8ea49aeac882ceb24021a3000a8d17ec114a12d
Adrien <adrien.poindron@gmail.com>
2025-03-21 16:07:06 +0100

    Update 41 job_start.sh

--- 40 Slurm cluster/41 job_start.sh
+++ 40 Slurm cluster/41 job_start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Name of the job
 #SBATCH --job-name=NewDesign
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=48
-#SBATCH --mem-per-cpu=2625
-#SBATCH --partition=wcpu
+
+# These commands choose how much ressources are allocated
+#SBATCH --nodes=1    # number of nodes 
+#SBATCH --ntasks=1   # number of cores
+#SBATCH --cpus-per-task=48  # number of cores
+#SBATCH --mem-per-cpu=2625  # MB of RAM allocated by cores (ntasks)
+#SBATCH --partition=wcpu    # partition where to run the program
 
 # activate conda env for python
 . "/swdata/poindron/miniconda3/etc/profile.d/conda.sh"
